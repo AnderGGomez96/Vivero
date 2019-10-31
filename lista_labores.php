57d518677497a507a5057a58ae98e2e95e344b1d
AnderGGomez96 <anderggomez96@gmail.com>
2019-10-30 19:29:31 -0500

    Se añaden funcionalidades

---
+++ lista_labores.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Lista de Cultivos Activos</title>
+	</head>
+	<body>
+
+
+		<div>
+			<table border="1">
+				<tr align="center"><td><label>Codigo labor</label></td>
+					<td><label>Nombre</label></td></tr>
+
+					<?php
+
+						require('conexion.php');
+						$query="SELECT * FROM labores WHERE eliminar = 0 ORDER BY codigo_labor";
+						$resultado=mysqli_query($link,$query);
+
+						while ($extraido= mysqli_fetch_array($resultado)) {
+							echo "<tr align='center'><td>".$extraido['codigo_labor']."</td>";
+							echo "<td>".$extraido['nombre']."</td></tr>";
+						}
+					?>
+			</table>
+			<input type="button" name="insert" value="Insertar" onclick="window.location.href='insertar_alumnos.php'">
+			<input type="button" name="delete" value="Eliminar" onclick="window.location.href='borrar_alumnos.php'">
+			<input type="button" name="volver" value="Volver" onclick="window.location.href='index.php'">
+		</div>
+	</body>
+</html>
